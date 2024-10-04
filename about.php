091ab483874f7ded4f1b0a41ddda8ea92b83d46a
Juste <juste.derk@gmail.com>
2024-10-04 17:22:43 +0200

    Initialize about.php file with information about the creator

--- about.php
+++ about.php
@@ -1,1 +1,26 @@
+<?php
 
+require __DIR__ . '/resources/header.php'; ?>
+
+<main>
+    <h1>About Me - The Creator</h1>
+    <p>Hi! I’m Juste Derkintyte, and I’ve recently embarked on my journey
+        to learn web development. With a background in various service jobs
+        and a career as an athlete, my interest in technology and
+        programming has always been a part of my life. Growing up, I
+        enjoyed playing video games and solving computer problems with
+        my dad. This passion led me to explore my skills in the engineering
+        program at Umeå University. However, after a year, I realized
+        something was missing.
+
+        I found myself repeatedly visiting the Yrgo Web Developer page,
+        unsure if I would even get accepted due to the competitive nature
+        of the program. To my surprise, I was accepted, and now I proudly
+        say I’m one step closer to becoming a web developer.
+
+        In addition to my studies, I pursue my passion for beach volleyball,
+        a sport that has shaped my life and taught me valuable lessons
+        in communication and teamwork. I look forward to the exciting
+        journey ahead in web development!
+    </p>
+</main>
