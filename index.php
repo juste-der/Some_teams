2b267f50d9f4a9a83f064e76633a890b382d5174
Juste <juste.derk@gmail.com>
2024-10-04 16:54:35 +0200

    Initialize project with basic file structure

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 
 require __DIR__ . '/data.php';
+require __DIR__ . '/resources/header.php';
+
+?>
+
+<main>
+    <h1>The Football Team Page</h1>
+    <?php require __DIR__ . '/resources/team-list.php'; ?>
+</main>
+
+<?php require __DIR__ . '/resources/footer.php'; ?>
