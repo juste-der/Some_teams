03693038f9ae87248e888771fbb12ee301484548
Juste <juste.derk@gmail.com>
2024-10-08 14:42:37 +0200

    Add container for teams with card as flexbox

--- resources/header.php
+++ resources/header.php
@@ -5,10 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Football Teams</title>
+    <link rel="stylesheet" href="/CSS/styles.css">
 </head>
 
 <body>
-    <header>
+    <header class="site-header">
         <nav>
             <a href="index.php">Home</a>
             <a href="about.php">About</a>
--- resources/team-list.php
+++ resources/team-list.php
@@ -6,29 +6,26 @@ require __DIR__ . '/../data.php';
 $ranking_order = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 17, 26, 31, 38, 'Not ranked'];
 ?>
 
-<div class="teams">
+<div class="teams-container">
     <?php
     foreach ($ranking_order as $ranking):
         foreach ($teams as $team_name => $team_info):
             //Check if teams ranking match the correct order
             if ($team_info['uefa-coefficient-ranking'] == $ranking) { ?>
-                <h2 class="team_name">
-                    <?= $team_name; ?>
-                </h2>
-                <h3 class="team_league">
-                    <?= "League: " . $team_info['league']; ?>
-                </h3>
-                <h3 class="team_ranking">
-                    <?= "Ranking: " . $team_info['uefa-coefficient-ranking']; ?>
-                </h3>
-                <h3 class="team_city">
-                    <?= "City: " . $team_info['city']; ?>
-                </h3>
-                <a href="<?= $team_info['url']; ?>"><?= "Official " . $team_name . " Website"; ?></a>
-                <img src="<?= $team_info['logo']; ?>" alt="">
-                <h3 class="team_group">
-                    <?= "Group: " . $team_info['group']; ?>
-                </h3>
+                <div class="team-card">
+                    <h2 class="team_name">
+                        <?= $team_name; ?>
+                    </h2>
+                    <img src="<?= $team_info['logo']; ?>" alt="">
+
+                    <div class="team-info">
+                        <p class="team_league"><?= "League: " . $team_info['league']; ?></p>
+                        <p class="team_ranking"><?= "Ranking: " . $team_info['uefa-coefficient-ranking']; ?></p>
+                        <p class="team_city"><?= "City: " . $team_info['city']; ?></p>
+                        <p class="team_group"><?= "Group: " . $team_info['group']; ?></p>
+                        <a href="<?= $team_info['url']; ?>"><?= "Official " . $team_name . " Website"; ?></a>
+                    </div>
+                </div>
     <?php
             }
         endforeach;
