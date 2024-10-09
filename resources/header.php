f2ddf808b2157b287f42df71799c4729ce51651f
Juste <juste.derk@gmail.com>
2024-10-09 09:48:11 +0200

    fixed how styles.css is connected to header.php

--- resources/header.php
+++ resources/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Football Teams</title>
-    <link rel="stylesheet" href="/CSS/styles.css">
+    <link rel="stylesheet" href="CSS/styles.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" rel="stylesheet">
@@ -14,7 +14,7 @@
     <header class="site-header">
         <nav>
             <a href="index.php" class="logo">
-                <img src="/images/Group 1.png" alt="My logo">
+                <img src="images/Group 1.png" alt="My logo">
             </a>
             <a href="index.php">Home</a>
             <a href="about.php">About</a>
