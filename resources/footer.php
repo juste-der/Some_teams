e0c349d96e20e1cea73041b9a4285a41a8a80f94
Juste <juste.derk@gmail.com>
2024-10-09 07:40:36 +0200

    Initialized footer class with navigations and styled

--- resources/footer.php
+++ resources/footer.php
@@ -1,3 +1,10 @@
-<footer>
+<footer class="site-footer">
+
+    <div class="footer-navigation">
+        <a href="https://www.facebook.com/?locale=sv_SE">Facebook</a>
+        <a href="https://www.instagram.com/">Instagram</a>
+        <a href="index.php">Memel.se</a>
+        <a href="index.php">Contact</a>
+    </div>
 
 </footer>
